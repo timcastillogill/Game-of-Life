f2ee04d087208c468c36756be8239d608f1258c8
Tim Castillo-Gill <timgillviola@gmail.com>
2021-02-09 17:12:56 +0000

    Initialising game with live cells

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,20 +1,21 @@
 # live cell = "x"
 # dead cell = nil
-
 # [
 #   [x,x,x], 
 #   [x,x,x], 
 #   [x,x,x]
 # ]
-
 class GameOfLife
-
   attr_reader :board
-
-  def initialize
-    @board = Array.new(3) {Array.new(3)}
+  def initialize(coordinates)
+    @board = create_board(coordinates)
   end
-
-
+  private
+  def create_board(coordinates)
+    empty_board = Array.new(3) { Array.new(3) }
+    coordinates.each do |x, y|
+      empty_board[x][y] = 'x'
+    end
+    empty_board
+  end
 end
-
--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,25 +1,26 @@
 require 'game_of_life'
-
-describe 'GameOfLife' do
-  
+describe GameOfLife do
+  subject(:game_of_life) { described_class.new(coordinates) }
   describe '#board' do
-
     context 'when I start the game' do
       context 'when I do not pass any coordinates' do
+        let(:coordinates) { [] }
         it 'returns an empty board' do
-          game = GameOfLife.new
-          expect(game.board).to eq [[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]] 
+          expect(game_of_life.board).to eq [[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]] 
         end
       end
-
       context 'when I pass it some coordinates' do
+        let(:coordinates) { [[0, 0], [1, 0], [2, 0], [2, 2]] }
         it 'returns a board with live cells' do
-          expect(GameOfLife.new).to eq [['x', nil, nil], ['x', nil, nil], ['x', nil, nil]] 
+          expect(game_of_life.board).to eq(
+            [
+              ['x', nil, nil],
+              ['x', nil, nil],
+              ['x', nil, 'x']
+            ]
+          )
         end
       end
     end
-
-    context ''
   end
-
 end
