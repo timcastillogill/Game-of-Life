35d816ba55a1848df3a510d82601719631941a23
Emily Worrall <emily.worrall@futurelearn.com>
2021-02-17 18:03:21 +0000

    Add pending feature specs for cells surviving
    This commit adds 2 pending feature specs which test that a live cell
    survives to the next tick, if it has 2 or 3 neighbours.

--- spec/features/survive_until_next_tick_test.rb
+++ spec/features/survive_until_next_tick_test.rb
@@ -1,49 +1,48 @@
 require 'game_of_life'
+require 'spec_helper'
 
-describe 'Surviving' do
-
+describe 'Surviving', type: :feature do
   scenario 'Any live cell with 2 live neighbours survives until next tick.' do
     pending
+
     given_i_have_a_cell_with_two_neighbours
     when_i_advance_to_the_next_tick
-    then_my_cells_survive
+    then_my_original_live_cell_survives
   end
 
   scenario 'Any live cell with 3 live neighbours survives until next tick.' do
     pending
+
     given_i_have_a_cell_with_three_neighbours
     when_i_advance_to_the_next_tick
-    then_my_cells_survive
+    then_my_original_live_cell_survives
   end
 
   private
 
   def given_i_have_a_cell_with_two_neighbours
-    coordinates = [
-      [0, 0], 
-      [0, 1], 
-      [1, 0], 
-    ]
+    @original_cell_coordinates = [0, 0]
+
+    coordinates = [@original_cell_coordinates, [0, 1], [1, 0]]
     @game = GameOfLife.new(coordinates)
   end
 
   def given_i_have_a_cell_with_three_neighbours
-    coordinates = [
-      [1, 1], 
-      [0, 1], 
-      [1, 0],
-      [1, 2],
-    ]
+    @original_cell_coordinates = [1, 1]
+
+    coordinates = [@original_cell_coordinates, [0, 1], [1, 0], [1, 2]]
     @game = GameOfLife.new(coordinates)
   end
 
   def when_i_advance_to_the_next_tick
-    binding.pry
+    @game.advance_to_next_tick
   end
 
-  def then_my_cells_survive
+  def then_my_original_live_cell_survives
+    x_coordinate = @original_cell_coordinates.first
+    y_coordinate = @original_cell_coordinates.last
 
+    expect(@game.board[x_coordinate][y_coordinate]).to eq('x')
   end
-
 end
 
